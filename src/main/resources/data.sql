8b2e1c1a1db3004035ada7f64f39493aa83b21d5
Stuart <stuart@planetcoops.com>
2022-11-03 15:35:18 +0000

    Add codecov.io

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO category (description) VALUES ('American');
-INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('ItalianAdd');
 INSERT INTO category (description) VALUES ('Mexican');
 INSERT INTO category (description) VALUES ('Fast Food');
 INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
